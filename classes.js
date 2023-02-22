8fe520b4e3e036848a12782c1955fcfa41daeec1
Vivek Goswami <54740376+falconfused@users.noreply.github.com>
2023-02-22 12:34:57 +0530

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,6 +1,7 @@
+
 let dataset = [
     {
-        "track": "Programming- Basics to Advanced",
+        "name": "Programming- Basics to Advanced",
         "id": 1,
         "courses": [
             {
@@ -2098,3 +2099,226 @@ let dataset = [
         ]
     }
 ]
+
+class Track {
+    id;
+    name;
+    courseList = [];
+    constructor(track) {
+        this.name = track.name
+        this.id = track.id
+        track.courses.map(
+            (courses) => {
+                this.courseList.push(new Course(courses))
+            }
+        )
+    }
+};
+
+
+class Course {
+    id;
+    name;
+    topicsList = [];
+    constructor(course) {
+        this.name = course.name
+        this.id = course.id
+        course.topics.map(
+            (topic) => {
+                this.topicsList.push(new Topic(topic))
+            }
+        )
+    }
+};
+
+
+class Topic {
+    id;
+    name;
+    setsList = [];
+    constructor(topic) {
+        this.name = topic.name
+        this.id = topic.id
+        topic.sets.map(
+            (set) => {
+                if (set.type == "McqSets")
+                    this.setsList.push(new McqSet(set))
+                if (set.type == "PracticeSets")
+                    this.setsList.push(new PracticeSet(set))
+                if (set.type == "LearningSets")
+                    this.setsList.push(new LearningSet(set))
+                if (set.type == "AssgnmentSets")
+                    this.setsList.push(new AssignmentSet(set))
+                if (set.type == "CodingSets")
+                    this.setsList.push(new CodingSet(set))
+
+            }
+        )
+    }
+};
+
+class Set {
+    id;
+    name;
+    type;
+    content;
+    constructor(set) {
+        this.id = set.id;
+        this.name = set.name
+        this.type = set.type
+        this.content = set
+    }
+}
+
+
+class McqSet extends Set {
+
+    constructor(set) {
+        super(set)
+    }
+
+
+    getInstuctions(index) {
+        console.log(content.instructions[index])
+    }
+    skipQuestion() {
+        console.log("question has been skipped")
+    }
+    getAnswer() {
+        console.log(content.answers[index])
+    }
+
+}
+class CodingSet extends Set {
+
+    constructor(set) {
+        super(set)
+    }
+    getQuestions(index) {
+        console.log(content.questions[index])
+    }
+    getDetails(index) {
+        console.log(content.details[index])
+    }
+    codeSubmit() {
+        console.log("Code has been submitted")
+    }
+    codeRun() {
+        console.log("Code is running")
+    }
+    getSolution() {
+        console.log(content.solutions[index])
+
+    }
+    gethint(index) {
+        console.log(content.hints[index])
+    }
+    gethint(index) {
+        console.log(content.discussions[index])
+    }
+    codeReset() {
+        console.log("Code has been reset")
+    }
+    codeSave() {
+        console.log("Code has been saved")
+    }
+}
+class LearningSet extends Set {
+
+    constructor(set) {
+        super(set)
+    }
+
+    getVideoDetails() {
+        console.log(videodetails)
+    }
+
+    getDiscussions(index) {
+        console.log(content.discussions[index])
+    }
+    markDone() {
+        console.log("the question is completed")
+    }
+    sendFeedback() {
+        console.log(feedback)
+    }
+    askDoubt() {
+        doubt = "";
+    }
+}
+class AssignmentSet extends Set {
+
+    constructor(set) {
+        super(set)
+    }
+    getQuestions(index) {
+        console.log(content.questions[index])
+    }
+    getDetails(index) {
+        console.log(content.details[index])
+    }
+    codeSubmit() {
+        console.log("Code has been submitted")
+    }
+    codeRun() {
+        console.log("Code is running")
+    }
+    getSolution() {
+        console.log(content.solutions[index])
+
+    }
+    getHint(index) {
+        console.log(content.hints[index])
+    }
+    getDiscussions(index) {
+        console.log(content.discussions[index])
+    }
+    codeReset() {
+        console.log("Code has been reset")
+    }
+    codeSave() {
+        console.log("Code has been saved")
+    }
+}
+class PracticeSet extends Set {
+
+    constructor(set) {
+        super(set)
+    }
+
+
+    getQuestions(index) {
+        console.log(content.questions[index])
+    }
+    getDetails(index) {
+        console.log(content.details[index])
+    }
+    codeSubmit() {
+        console.log("Code has been submitted")
+    }
+    codeRun() {
+        console.log("Code is running")
+    }
+    getSolution() {
+        console.log(content.solutions[index])
+
+    }
+    gethint(index) {
+        console.log(content.hints[index])
+    }
+    getDiscussions(index) {
+        console.log(content.discussions[index])
+    }
+    codeReset() {
+        console.log("Code has been reset")
+    }
+    codeSave() {
+        console.log("Code has been saved")
+    }
+}
+
+
+let track = new Track(dataset[0])
+
+
+console.log(track)
